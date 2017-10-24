eb9868df686dd631e43babe35e76d214a1163498
Rômulo Rossi <romulomrossi@hotmail.com>
2017-10-24 09:09:36 -0200

    added codeIgniter Files

---
+++ woodenblock/application/views/common/footer.php
@@ -0,0 +1,31 @@
+      <footer class="page-footer teal lighten-2">
+          <div class="container">
+            <div class="row">
+              <div class="col l6 s12">
+                <h5 class="white-text">WoodenBlock</h5>
+                <p class="grey-text text-lighten-4">Encontre profissionais experientes para contribuir com suas tarefas!</p>
+              </div>
+              <div class="col l4 offset-l2 s12">
+                <ul>
+                  <li><a class="grey-text text-lighten-3" href="#!">Tarefas</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Menu</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Colaboradores</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Sobre</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="footer-copyright ">
+            <div class="container">
+            ©2017WOODENBLOCKS
+            </div>
+          </div>
+        </footer>
+
+
+     <!--Import jQuery before materialize.js-->
+      <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+      <script type="text/javascript" src="js/materialize.js"></script>
+
+    </body>
+  </html>
---
+++ woodenblock/application/views/common/header.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <!-- Compiled and minified CSS -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
+
+  <!-- Compiled and minified JavaScript -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
+    <!-- Compiled and minified CSS -->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <meta charset="UTF-8"></meta>
+    <title>WOODENBLOCKS</title>
+
+</head>
+
+<body>
+
+    <!-- Dropdown Structure -->
+    <ul id="dropdown1" class="dropdown-content">
+        <li><a href="login.php">Entrar</a></li>
+        <li class="divider"></li>
+        <li><a href="register.php">Registrar</a></li>
+    </ul>
+    <nav>
+        <div class="nav-wrapper teal lighten-2">
+            <a href="#!" class="brand-logo">WOODENBLOCKS</a>
+            <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
+            <ul class="right hide-on-med-and-down">
+                <li><a href="home.php">HOME</a></li>
+                <li><a href="profile.php">PERFIL</a></li>
+                <li><a href="tarefas.php">TAREFAS</a></li>
+                <!-- Dropdown Trigger -->
+                <li><a class="dropdown-button" href="#!" data-activates="dropdown1">ENTRAR<i class="material-icons right">arrow_drop_down</i></a></li>
+            </ul>
+            <ul class="side-nav teal lighten-4" id="mobile-demo">
+                <li><a href="home.php">HOME</a></li>
+                <li class="divider"></li>
+                <li><a href="profile.php">PERFIL</a></li>
+                <li class="divider"></li>
+                <li><a href="tarefas.php">TAREFAS</a></li>
+                <li class="divider"></li>
+                <li><a href="login.php">ENTRAR</a></li>
+                <li><a href="register.php">REGISTRAR</a></li>
+            </ul>
+        </div>
+    </nav>
+
+
+    <style>
+        nav{
+            margin-bottom: 15%;
+        }
+        footer{
+            margin-top:15%;
+        }
+    </style>
