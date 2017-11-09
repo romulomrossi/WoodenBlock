38c6f410f041086a66eb828e823f040872e3e668
Antony <anthonylcassol@gmail.com>
2017-11-09 11:26:20 -0200

    alterei javascript

--- woodenblock/application/views/common/footer.php
+++ woodenblock/application/views/common/footer.php
@@ -25,7 +25,14 @@
 
      <!--Import jQuery before materialize.js-->
       <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
-      <script src="<?php echo $this->base_url();?>assets/javascript/materialize.js"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
       
     </body>
   </html>
+  
+  <script>
+	$(document).ready(function(){
+      // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
+      $('.modal').modal();
+    });
+  </script>
